17ca3fe2e9c8c2e0e2a77a7bdb8cb5510d38e80b
Andrew Vozniak <avozniaka@gmail.com>
2023-03-04 10:35:26 +0200

    Add github secrets

--- config/config.exs
+++ config/config.exs
@@ -51,8 +51,8 @@ config :ueberauth, Ueberauth,
   providers: [github: {Ueberauth.Strategy.Github, [default_scope: "user:email"]}]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: "14f92c4f48588d2a88e6",
-  client_secret: "6aade2ef2813fccaeb6c22a51b9032c1a8b974f1"
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
